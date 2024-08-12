f6c6e4b0dd962400195a98f4260888f44f7edf90
rennanimael <imael.rennan@ufms.br>
2024-08-11 20:58:12 -0400

    estilizacao e responsividade adicionada

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -12,34 +12,38 @@ export default function MovieCard(props: Props) {
         <li className = 'movie-card'>
             <div className="movie-poster">
                 <img
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt="{movie.title}"
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    alt="{movie.title}"
                 />
             </div>
 
-            <div className="movie-infos">
-
-
-            </div>
-
-            <p className="movie-title">
-                {movie.title}                       
-            </p>
-                <StarRating 
-                    rating ={movie.vote_average}
-                />
-            <div className="hidden-content">
-                <p className = 'description'>
-                    {movie.overview}
+            <div className="movie-infos">   
+                <p className="movie-title">
+                    {movie.title}                       
                 </p>
 
-                <button className="btn-default">
-                    Ver mais
-                </button>
-            </div>                    
-            
+                {movie.vote_average > 0 &&
+                    <StarRating 
+                    rating ={movie.vote_average}
+                    />
+                }
+                
+                <div className="hidden-content">
+                    {movie.overview &&
+                        <p className = 'description'>
+                          {movie.overview.length > 100
+                             ? `${movie.overview.substring(0, 100)}...`
+                             :movie.overview
+                          }
+                        </p> 
+                    }
+                  
 
-           
+                    <button className="btn-default">
+                        Ver mais
+                    </button>
+                </div>                    
+            </div>  
         </li>
     )
 }
--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -5,16 +5,17 @@ import './index.scss';
 import axios from 'axios';
 import MovieCard from '../MovieCard';
 import { Movie } from '@/Types/movie';
+import ReactLoading from 'react-loading'
 
 export default function MovieList() {
     const [movies, setMovies] = useState<Movie[]>([]);
-
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     useEffect(() =>{
         getMovies();
     }, [])
 
-    const getMovies = () =>{
-        axios({
+    const getMovies = async () =>{
+        await axios({
             method: 'get',
             url: 'https://api.themoviedb.org/3/discover/movie',
             params: {
@@ -24,8 +25,18 @@ export default function MovieList() {
         }).then(response => {
             setMovies(response.data.results);            
         })
+
+        setIsLoading(false);
     }
 
+    if(isLoading){
+        return (
+            <div className = 'loading-container'>
+                <ReactLoading type="spin" color="#6046ff" height={'5%'} width={'5%'} />
+
+            </div>
+        )
+    }
     return (
         <ul className="movie-list">
             {movies.map((movie) =>
